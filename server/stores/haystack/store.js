07792798807695d9b8fca89409a11d105eb53b09
vsen <vsen@expedia.com>
2017-09-25 16:16:14 +0530

    grpc client code to connect with trace reader

--- server/stores/haystack/store.js
+++ server/stores/haystack/store.js
@@ -1,4 +1,3 @@
-
 /*
 
  *  Copyright 2017 Expedia, Inc.
@@ -19,12 +18,127 @@
  */
 
 const Q = require('q');
+const grpc = require('grpc');
+
+const config = require('../../config/config');
+const services = require('../../../static_codegen/traceReader_grpc_pb');
+const messages = require('../../../static_codegen/traceReader_pb');
+const searchResultsTransformer = require('./searchResultsTransformer');
+const rangeConverter = require('../utils/rangeConverter');
 
 const store = {};
 
-store.getServices = () => Q.fcall(() => []);
-store.getOperations = () => Q.fcall(() => []);
-store.findTraces = () => Q.fcall(() => []);
-store.getTrace = () => Q.fcall(() => []);
+const client = new services.TraceReaderClient(
+    `${config.store.haystack.host}:${config.store.haystack.port}`,
+    grpc.credentials.createInsecure());
+
+const reservedField = ['timePreset', 'startTime', 'endTime'];
+
+function createFieldsList(query) {
+  return Object.keys(query)
+      .filter(key => query[key] && !reservedField.includes(key))
+      .map((key) => {
+        const field = new messages.Field();
+        field.setName(key);
+        field.setValue(query[key]);
+
+        return field;
+      });
+}
+
+// TODO improve logging, monitoring and return correct status code for endpoints below
+store.getServices = () => {
+  const deferred = Q.defer();
+
+  const request = new messages.FieldValuesRequest();
+  request.setFieldname('service');
+
+  client.getFieldValues(request, (result) => {
+    if (result.Error || !result.getFieldValues) {
+      deferred.reject({});
+    } else {
+      deferred.resolve(result.getFieldValues());
+    }
+  });
+
+  return deferred.promise;
+};
+
+store.getOperations = (serviceName) => {
+  const deferred = Q.defer();
+
+  const service = new messages.Field();
+  service.setName('service');
+  service.setValue(serviceName);
+
+  const request = new messages.FieldValuesRequest();
+  request.setFieldname('operation');
+  request.setFiltersList(new messages.Field());
+
+  client.getFieldValues(request, (result) => {
+    if (result.Error || !result.getFieldValues) {
+      deferred.reject({});
+    } else {
+      deferred.resolve(result.getFieldValues());
+    }
+  });
+
+  return deferred.promise;
+};
+
+store.getTrace = (traceId) => {
+  const deferred = Q.defer();
+
+  const request = new messages.TraceRequest();
+  request.setTraceid(traceId);
+
+  client.getTrace(request, (result) => {
+    if (result.Error || !result.getTrace) {
+      deferred.reject({});
+    } else {
+      deferred.resolve(result.getTrace());
+    }
+  });
+
+  return deferred.promise;
+};
+
+store.findTraces = (query) => {
+  const deferred = Q.defer();
+
+  if (query.traceId) {
+    // if search is for a singe trace, perform getTrace instead of search
+    const request = new messages.TraceRequest();
+    request.setTraceid(query.traceId);
+
+    client.getTrace(request, (result) => {
+      if (result.Error || !result.getTrace) {
+        deferred.reject({});
+      } else {
+        deferred.resolve(
+            searchResultsTransformer.transform([result.getTrace()], query));
+      }
+    });
+  } else {
+    const request = new messages.TracesSearchRequest();
+    request.setFieldsList(createFieldsList(query));
+    request.setStarttime((query.startTime && parseInt(query.startTime, 10))
+        || ((Date.now() * 1000) - rangeConverter.toDuration(query.timePreset)));
+    request.setEndtime((query.endTime && parseInt(query.endTime, 10))
+        || Date.now() * 1000);
+    request.setLimit(40);
+
+    client.searchTraces(request, (result) => {
+      if (result.Error || !result.getTrace) {
+        deferred.reject({});
+      } else {
+        deferred.resolve(
+            searchResultsTransformer.transform(result.getTracesList(), query));
+      }
+    });
+  }
+
+  return deferred.promise;
+};
 
 module.exports = store;
