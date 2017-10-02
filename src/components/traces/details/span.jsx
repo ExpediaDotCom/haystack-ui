1ade02ebba3cfffe7f2827a3caae438edce42789
Jason Bulicek <jbulicek@expedia.com>
2017-10-02 13:22:47 -0700

    attempted fix for travis

--- src/components/traces/details/span.jsx
+++ src/components/traces/details/span.jsx
@@ -181,6 +181,7 @@ export default class Span extends React.Component {
                 x2={`${parentOffsetPercent}%`}
                 y1={(parentIndex * spanHeight) + timePointersHeight + (verticalPadding * 3.8)}
                 y2={horizontalLineY}
+                strokeDasharray="3, 5"
             />
             <line
                 className="invocation-line"
@@ -188,6 +189,7 @@ export default class Span extends React.Component {
                 x2={`${leftOffsetPercent}%`}
                 y1={horizontalLineY}
                 y2={horizontalLineY}
+                strokeDasharray="3, 5"
             />
         </g>);
 
