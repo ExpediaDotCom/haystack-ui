ee0223f716c1d4f5b584ab777fc0b0500554014d
Abhishek Srivastava <absrivastava@expedia.com>
2017-11-07 20:31:15 +0530

    handling undefined config

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -21,7 +21,7 @@ import PropTypes from 'prop-types';
 
 import './header.less';
 
-const rootService = window.haystackUiConfig.rootService || undefined;
+const rootService = window.haystackUiConfig ? window.haystackUiConfig.rootService : undefined;
 
 class SearchBar extends React.Component {
     static propTypes = {
