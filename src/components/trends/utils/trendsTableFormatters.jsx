8c3ac05c2d441d8083ec76c5b1d29bfddbd6fae9
Jason Bulicek <jbulicek@expedia.com>
2018-01-23 09:01:28 -0800

    add small fix to rendering 0 value for latest duration

--- src/components/trends/utils/trendsTableFormatters.jsx
+++ src/components/trends/utils/trendsTableFormatters.jsx
@@ -53,7 +53,7 @@ export default class TrendsTableFormatters {
         return (<div className="sparkline-container">
             { cell &&
             <div className="sparkline-title">
-                total <b>{formatters.toNumberString(cell)}</b>
+                total: <b>{formatters.toNumberString(cell)}</b>
             </div>}
             <div className="sparkline-graph">
                 <Sparklines className="sparkline" data={values} min={0} height={48}>
@@ -67,9 +67,9 @@ export default class TrendsTableFormatters {
         const values = row.tp99DurationPoints.map(d => d.value);
 
         return (<div className="sparkline-container">
-            { cell &&
+            { Number.isInteger(cell) &&
             <div className="sparkline-title">
-                latest <b>{formatters.toDurationStringFromMs(cell)}</b>
+                latest: <b>{formatters.toDurationStringFromMs(cell)}</b>
             </div>}
             <div className="sparkline-graph">
                 <Sparklines className="sparkline" data={values} min={0} height={48}>
@@ -96,7 +96,7 @@ export default class TrendsTableFormatters {
         return (<div className="sparkline-container ">
             { cell &&
                 <div className={'sparkline-title'}>
-                    average <b className={`sparkline-percentColor ${stateColor}`}> {cell.toFixed(2)}% </b>
+                    average: <b className={`sparkline-percentColor ${stateColor}`}> {cell.toFixed(2)}% </b>
                 </div>
             }
             <div className="sparkline-graph">
