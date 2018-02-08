bebbfc6db2bfa8b5e39a6fbae8b20006736bf803
vsen <vsen@expedia.com>
2018-02-08 18:42:19 +0530

    stacking success and failure lines in count graphs

--- src/components/trends/details/graphs/durationGraph.jsx
+++ src/components/trends/details/graphs/durationGraph.jsx
@@ -57,33 +57,34 @@ const DurationGraph = ({meanPoints, tp95Points, tp99Points}) => {
     }
 
     const chartData = {
-        datasets: [{
-            label: 'Mean     ',
-            data: meanData,
-            backgroundColor: backgroundColor1,
-            borderColor: borderColor1,
-            borderWidth: 1,
-            pointRadius: 1,
-            pointHoverRadius: 3
-        },
-        {
-            label: 'TP95    ',
-            data: tp95Data,
-            backgroundColor: backgroundColor2,
-            borderColor: borderColor2,
-            borderWidth: 1,
-            pointRadius: 1,
-            pointHoverRadius: 3
-        },
-        {
-            label: 'TP99    ',
-            data: tp99Data,
-            backgroundColor: backgroundColor3,
-            borderColor: borderColor3,
-            borderWidth: 1,
-            pointRadius: 1,
-            pointHoverRadius: 3
-        }]
+        datasets: [
+            {
+                label: 'TP99    ',
+                data: tp99Data,
+                backgroundColor: backgroundColor3,
+                borderColor: borderColor3,
+                borderWidth: 1,
+                pointRadius: 1,
+                pointHoverRadius: 3
+            },
+            {
+                label: 'TP95    ',
+                data: tp95Data,
+                backgroundColor: backgroundColor2,
+                borderColor: borderColor2,
+                borderWidth: 1,
+                pointRadius: 1,
+                pointHoverRadius: 3
+            },
+            {
+                label: 'Mean     ',
+                data: meanData,
+                backgroundColor: backgroundColor1,
+                borderColor: borderColor1,
+                borderWidth: 1,
+                pointRadius: 1,
+                pointHoverRadius: 3
+            }]
     };
 
     return (<div className="col-md-12">
