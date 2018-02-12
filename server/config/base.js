448dbf48e5bd4edba7f16e464cad1aef4d15975d
Abhishek Srivastava <absrivastava@expedia.com>
2018-02-12 21:10:01 +0530

    updating base config

--- server/config/base.js
+++ server/config/base.js
@@ -27,7 +27,8 @@ module.exports = {
             //             zipkin store expects a zipkin config field specifying zipking api url,
             //             eg. zipkinUrl: 'http://<zipkin>/api/v1'}
             //  - stub - a stub used during development, will be removed in future
-            storeName: 'stub'
+            storeName: 'stub',
+            fieldKeys: ['traceId', 'error', 'minDuration']
         },
         trends: {
             // name of config store module to use for fetching trends data from downstream
