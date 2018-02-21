01b913dd2894fa60a0d5e3eb1ab61004b0d8c89c
Jason Bulicek <jbulicek@expedia.com>
2018-02-21 08:45:38 -0800

    update copywrite in some files

--- src/components/traces/stores/traceTrendFetcher.js
+++ src/components/traces/stores/traceTrendFetcher.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Expedia, Inc.
+ * Copyright 2018 Expedia, Inc.
  *
  *         Licensed under the Apache License, Version 2.0 (the "License");
  *         you may not use this file except in compliance with the License.
