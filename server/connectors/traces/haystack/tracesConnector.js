f68261c6584601c4ce1ffe153e97d9971a0ea827
vsen <vsen@expedia.com>
2018-02-26 12:22:08 +0530

    adding grpc fetcher for traces

--- server/connectors/traces/haystack/tracesConnector.js
+++ server/connectors/traces/haystack/tracesConnector.js
@@ -17,50 +17,31 @@
 
  */
 
-const Q = require('q');
-const grpc = require('grpc');
-
-const config = require('../../../config/config');
-const services = require('../../../../static_codegen/traceReader_grpc_pb');
 const messages = require('../../../../static_codegen/traceReader_pb');
 const searchResultsTransformer = require('./searchResultsTransformer');
-const errorConverter = require('../../utils/errorConverter');
 const protobufConverter = require('./protobufConverter');
 const searchRequestBuilder = require('./searchRequestBuilder');
 const objectUtils = require('../../utils/objectUtils');
+const fetcher = require('../../fetchers/grpcFetcher');
+
+const fieldValueFetcher = fetcher('getFieldValues');
+const traceFetcher = fetcher('getTrace');
+const rawTraceFetcher = fetcher('getRawTrace');
+const rawSpanFetcher = fetcher('getRawSpan');
+const tracesSearchFetcher = fetcher('searchTraces');
 
 const connector = {};
 
-const client = new services.TraceReaderClient(
-    `${config.connectors.traces.haystackHost}:${config.connectors.traces.haystackPort}`,
-    grpc.credentials.createInsecure()); // TODO make client secure
-
-function generateCallDeadline() {
-    return new Date().setMilliseconds(new Date().getMilliseconds() + config.upstreamTimeout);
-}
-
 connector.getServices = () => {
-    const deferred = Q.defer();
-
     const request = new messages.FieldValuesRequest();
     request.setFieldname('serviceName');
 
-    client.getFieldValues(request,
-        {deadline: generateCallDeadline()},
-        (error, result) => {
-            if (error || !result) {
-                deferred.reject(errorConverter.fromGrpcError(error));
-            } else {
-                deferred.resolve(result.getValuesList());
-            }
-        });
-
-    return deferred.promise;
+    return fieldValueFetcher
+    .fetch(request)
+    .then(result => result.getValuesList());
 };
 
 connector.getOperations = (serviceName) => {
-    const deferred = Q.defer();
-
     const service = new messages.Field();
     service.setName('serviceName');
     service.setValue(serviceName);
@@ -70,79 +51,21 @@ connector.getOperations = (serviceName) => {
     request.setFiltersList(new messages.Field());
     request.setFiltersList([service]);
 
-    client.getFieldValues(request,
-        {deadline: generateCallDeadline()},
-        (error, result) => {
-            if (error || !result) {
-                deferred.reject(errorConverter.fromGrpcError(error));
-            } else {
-                deferred.resolve(result.getValuesList());
-            }
-        });
-
-    return deferred.promise;
+    return fieldValueFetcher
+    .fetch(request)
+    .then(result => result.getValuesList());
 };
 
 connector.getTrace = (traceId) => {
-    const deferred = Q.defer();
-
     const request = new messages.TraceRequest();
     request.setTraceid(traceId);
 
-    client.getTrace(request,
-        {deadline: generateCallDeadline()},
-        (error, result) => {
-            if (error || !result) {
-                deferred.reject(errorConverter.fromGrpcError(error));
-            } else {
-                deferred.resolve(protobufConverter.toTraceJson(messages.Trace.toObject(false, result)));
-            }
-        });
-
-    return deferred.promise;
-};
-
-connector.getRawTrace = (traceId) => {
-    const deferred = Q.defer();
-
-    const request = new messages.TraceRequest();
-    request.setTraceid(traceId);
-
-    client.getRawTrace(request,
-        {deadline: generateCallDeadline()},
-        (error, result) => {
-            if (error || !result) {
-                deferred.reject(errorConverter.fromGrpcError(error));
-            } else {
-                deferred.resolve(protobufConverter.toTraceJson(messages.Trace.toObject(false, result)));
-            }
-        });
-
-    return deferred.promise;
-};
-
-connector.getRawSpan = (traceId, spanId) => {
-    const deferred = Q.defer();
-
-    const request = new messages.SpanRequest();
-    request.setTraceid(traceId);
-    request.setSpanid(spanId);
-
-    client.getRawSpan(request,
-        {deadline: generateCallDeadline()},
-        (error, result) => {
-            if (error || !result) {
-                deferred.reject(errorConverter.fromGrpcError(error));
-            } else {
-                deferred.resolve(protobufConverter.toSpanJson(messages.Span.toObject(false, result)));
-            }
-        });
-
-    return deferred.promise;
+    return traceFetcher
+    .fetch(request)
+    .then(result => protobufConverter.toTraceJson(messages.Trace.toObject(false, result)));
 };
 
 connector.findTraces = (query) => {
-    const deferred = Q.defer();
     const traceId = objectUtils.getPropIgnoringCase(query, 'traceId');
 
     if (traceId) {
@@ -150,34 +73,43 @@ connector.findTraces = (query) => {
         const request = new messages.TraceRequest();
         request.setTraceid(traceId);
 
-        client.getTrace(request,
-            {deadline: generateCallDeadline()},
-            (error, result) => {
-                if (error || !result) {
-                    deferred.reject(errorConverter.fromGrpcError(error));
-                } else {
-                    const pbTrace = messages.Trace.toObject(false, result);
-                    const jsonTrace = protobufConverter.toTraceJson(pbTrace);
+        return traceFetcher
+        .fetch(request)
+        .then((result) => {
+            const pbTrace = messages.Trace.toObject(false, result);
+            const jsonTrace = protobufConverter.toTraceJson(pbTrace);
 
-                    deferred.resolve(searchResultsTransformer.transform([jsonTrace], query));
-                }
-            });
-    } else {
-        client.searchTraces(searchRequestBuilder.buildRequest(query),
-            {deadline: generateCallDeadline()},
-            (error, result) => {
-                if (error || !result) {
-                    deferred.reject(errorConverter.fromGrpcError(error));
-                } else {
-                    const pbTraceResult = messages.TracesSearchResult.toObject(false, result);
-                    const jsonTraceResults = pbTraceResult.tracesList.map(pbTrace => protobufConverter.toTraceJson(pbTrace));
-
-                    deferred.resolve(searchResultsTransformer.transform(jsonTraceResults, query));
-                }
-            });
+            return searchResultsTransformer.transform([jsonTrace], query);
+        });
     }
 
-    return deferred.promise;
+    return tracesSearchFetcher
+    .fetch(searchRequestBuilder.buildRequest(query))
+    .then((result) => {
+        const pbTraceResult = messages.TracesSearchResult.toObject(false, result);
+        const jsonTraceResults = pbTraceResult.tracesList.map(pbTrace => protobufConverter.toTraceJson(pbTrace));
+
+        return searchResultsTransformer.transform(jsonTraceResults, query);
+    });
+};
+
+connector.getRawTrace = (traceId) => {
+    const request = new messages.TraceRequest();
+    request.setTraceid(traceId);
+
+    return rawTraceFetcher
+    .fetch(request)
+    .then(result => protobufConverter.toTraceJson(messages.Trace.toObject(false, result)));
+};
+
+connector.getRawSpan = (traceId, spanId) => {
+    const request = new messages.SpanRequest();
+    request.setTraceid(traceId);
+    request.setSpanid(spanId);
+
+    return rawSpanFetcher
+    .fetch(request)
+    .then(result => protobufConverter.toSpanJson(messages.Span.toObject(false, result)));
 };
 
 module.exports = connector;
