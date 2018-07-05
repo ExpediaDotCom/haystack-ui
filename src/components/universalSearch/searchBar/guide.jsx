d6143db2597512676456f8f6b78672f3bcd56696
vsen <vsen@expedia.com>
2018-07-05 18:37:18 +0530

    improving guides

--- src/components/universalSearch/searchBar/guide.jsx
+++ src/components/universalSearch/searchBar/guide.jsx
@@ -17,8 +17,8 @@
 import React from 'react';
 
 export default () => (
-    <div className="usb-suggestions__helpers-wrapper pull-left">
-        <p>
+    <div className="usb-suggestions__guide-wrapper pull-left">
+        <section>
             <div><b>How to search</b></div>
             <div>Specify tag key value pairs (in whitespace separated key=value format) to search for traces containing give tags, eg:</div>
             <ul>
@@ -30,22 +30,36 @@ export default () => (
             <ul>
                 <li>Traces having testid=1 and having error in service test-svc span: <code>testid=1 (serviceName=test-svc error=true)</code></li>
             </ul>
-        </p>
-        <p>
+        </section>
+        <section>
             <div><b>How tabs show up</b></div>
             <div>
                 <ul>
-                    <li>If no tag searched - <span className="ti-vector"/><span> Service Graph</span></li>
-                    <li>If only serviceName (and/or operationName) searched - <span className="ti-align-left"/><span> Traces</span>, <span className="ti-stats-up"/><span> Trends</span>, <span className="ti-bell"/><span> Alerts</span></li>
-                    <li>Any other combination of tags searched - <span className="ti-align-left"/><span> Traces</span></li>
+                    <li>If no tag searched -
+                        <span className="usb-suggestions__guide-highlight">
+                            <span className="ti-vector usb-suggestions__guide-tab"/><span> Service Graph </span>
+                        </span>
+                    </li>
+                    <li>If only serviceName (and/or operationName) searched -
+                        <span className="usb-suggestions__guide-highlight">
+                            <span className="ti-align-left usb-suggestions__guide-tab"/><span> Traces </span>
+                            <span className="ti-stats-up usb-suggestions__guide-tab"/><span> Trends </span>
+                            <span className="ti-bell usb-suggestions__guide-tab"/><span> Alerts </span>
+                        </span>
+                    </li>
+                    <li>Any other combination of tags searched -
+                        <span className="usb-suggestions__guide-highlight">
+                            <span className="ti-align-left usb-suggestions__guide-tab"/><span> Traces </span>
+                        </span>
+                    </li>
                 </ul>
             </div>
-        </p>
-        <p>
-            <b>History</b>
+        </section>
+        <section>
+            <div><b>History</b></div>
             <ul>
                 <li><code>coming soon</code></li>
             </ul>
-        </p>
+        </section>
     </div>
 );
