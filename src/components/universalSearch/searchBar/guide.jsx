7a064702b5786a64b5da793ba57517e68e91b101
vsen <vsen@expedia.com>
2018-07-05 17:57:21 +0530

    updating guide

--- src/components/universalSearch/searchBar/guide.jsx
+++ src/components/universalSearch/searchBar/guide.jsx
@@ -19,19 +19,32 @@ import React from 'react';
 export default () => (
     <div className="usb-suggestions__helpers-wrapper pull-left">
         <p>
-            <b>Guide</b>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
-            <div>Examples</div>
+            <div><b>How to search</b></div>
+            <div>Specify tag key value pairs (in whitespace separated key=value format) to search for traces containing give tags, eg:</div>
             <ul>
-                <li><code>serviceName=test-service</code></li>
-                <li><code>testid=1 (serviceName=test-service error=true)</code></li>
+                <li>Traces having traceId xxxx :<code>traceId=xxxx</code></li>
+                <li>Traces passing through test-svc service: <code>serviceName=test-svc</code></li>
+                <li>Traces passing through test-svc service and having error in any of its spans: <code>serviceName=test-svc error=true</code></li>
             </ul>
+            <div>To target a specific span, enclose tag list in parenthesis, eg:</div>
+            <ul>
+                <li>Traces having testid=1 and having error in service test-svc span: <code>testid=1 (serviceName=test-svc error=true)</code></li>
+            </ul>
+        </p>
+        <p>
+            <div><b>How tabs show up</b></div>
+            <div>
+                <ul>
+                    <li>If no tag searched - <span className="ti-vector"/><span> Service Graph</span></li>
+                    <li>If only serviceName (and/or operationName) searched - <span className="ti-align-left"/><span> Traces</span>, <span className="ti-stats-up"/><span> Trends</span>, <span className="ti-bell"/><span> Alerts</span></li>
+                    <li>Any other combination of tags searched - <span className="ti-align-left"/><span> Traces</span></li>
+                </ul>
+            </div>
         </p>
         <p>
             <b>History</b>
             <ul>
-                <li><code>serviceName=test-service</code></li>
-                <li><code>testid=1 (serviceName=test-service error=true)</code></li>
+                <li><code>coming soon</code></li>
             </ul>
         </p>
     </div>
