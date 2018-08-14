87e2006c7788bc62ad82c88ed8bbd891a448978c
vinay sen <vsen@expedia.com>
2018-08-14 17:15:14 +0530

    Service graph improvements (#379)
    * adding missing useExpressionTree field
    
    * showing per in/out edge breakdown in service level service graph
    
    * styling improvements for graph info
    
    * making service list expandable
    
    * showing traffic split on tooltip
    
    * color coding error percentage
    
    * changing text for na

--- src/components/serviceGraph/serviceGraphResults.jsx
+++ src/components/serviceGraph/serviceGraphResults.jsx
@@ -49,17 +49,36 @@ export default class ServiceGraphResults extends React.Component {
         return {level: 'normal', severity: 0, errorRate};
     }
 
-    static createNoticeContent(requestRate, errorPercent, level) {
-        return `<table>
+    static createNoticeContent(node, incomingEdges) {
+        let incomingEdgesList = ['<tr><td>NA</td><td/><td/></tr>'];
+
+        if (incomingEdges.length) {
+            incomingEdgesList = incomingEdges.sort((a, b) => b.stats.count - a.stats.count).map((e) => {
+                const errorPercentage = (e.stats.errorCount * 100) / e.stats.count;
+                const level = ServiceGraphResults.getNodeDisplayDetails(errorPercentage);
+                return `
                     <tr>
-                        <td class="vizceral-notice__title">Incoming rq :</td>
-                        <td><b>${Number(requestRate).toFixed(2)}/sec</b></td>
-                    </tr>
-                    <tr>
-                        <td class="vizceral-notice__title">Error :</td>
-                        <td class="vizceral-${level}"><b>${Number(errorPercent).toFixed(2)}%</b></td>
-                    </tr>
-                </table>`;
+                        <td>${e.source.name}</td>
+                        <td class="text-right">${e.stats.count.toFixed(2)}</td>
+                        <td class="text-right service-graph__info-error-${level.level}">${errorPercentage.toFixed(2)}%</td>
+                    </tr>`;
+            });
+        }
+
+        return `
+                <h5>Traffic in <b>${node}</b></h5>
+                <div class="text-muted">(last 1 hour average)</div>
+                <table class="service-graph__info-table">
+                    <thead>
+                        <tr>
+                            <th>Service</th>
+                            <th class="text-right">Rq/Sec</th>
+                            <th class="text-right">Error%</th>
+                        </tr>
+                    </thead>
+                    <tbody>${incomingEdgesList.join('')}</tbody>
+                </table>
+            `;
     }
 
     static createNodes(graph) {
@@ -70,7 +89,7 @@ export default class ServiceGraphResults extends React.Component {
                 class: nodeDisplayDetails.level,
                 notices: [
                     {
-                        title: ServiceGraphResults.createNoticeContent(graph.requestRateForNode(node), graph.errorRateForNode(node), nodeDisplayDetails.level),
+                        title: ServiceGraphResults.createNoticeContent(node, graph.incomingTrafficForNode(node)),
                         severity: nodeDisplayDetails.severity
                     }
                 ]
@@ -185,39 +204,39 @@ export default class ServiceGraphResults extends React.Component {
 
         return (
             <div>
-            <article className="serviceGraph__panel">
-                <ServiceGraphSearch searchStringChanged={this.searchStringChanged} searchString={this.state.searchString} />
-                <Vizceral
-                    traffic={config}
-                    view={['haystack']}
-                    styles={style}
-                    definitions={definitions}
-                    allowDraggingOfNodes
-                    targetFramerate={60}
-                    objectHighlighted={this.objectHighlighted}
-                    match={this.state.searchString}
-                    viewChanged={this.setView}
-                />
                 {
-                    !!connDetails &&
-                    <ConnectionDetails
-                        requestRate={graph.requestRateForConnection(connDetails.split('--')[0], connDetails.split('--')[1])}
-                        errorPercent={graph.errorRateForConnection(connDetails.split('--')[0], connDetails.split('--')[1])}
-                        onClose={this.onConnectionDetailsClose}
+                    !!serviceName &&
+                    <NodeDetails
+                        serviceName={serviceName}
+                        requestRate={graph.requestRateForNode(serviceName)}
+                        errorPercent={graph.errorRateForNode(serviceName)}
+                        incomingEdges={graph.incomingTrafficForNode(serviceName)}
+                        outgoingEdges={graph.outgoingTrafficForNode(serviceName)}
+                        tags={graph.tagsForNode(serviceName)}
                     />
                 }
-            </article>
-            {
-                !!serviceName &&
-                <NodeDetails
-                    serviceName={serviceName}
-                    requestRate={graph.requestRateForNode(serviceName)}
-                    errorPercent={graph.errorRateForNode(serviceName)}
-                    incomingEdges={graph.incomingTrafficForNode(serviceName)}
-                    outgoingEdges={graph.outgoingTrafficForNode(serviceName)}
-                    tags={graph.tagsForNode(serviceName)}
-                />
-            }
+                <article className="serviceGraph__panel">
+                    <ServiceGraphSearch searchStringChanged={this.searchStringChanged} searchString={this.state.searchString} />
+                    <Vizceral
+                        traffic={config}
+                        view={['haystack']}
+                        styles={style}
+                        definitions={definitions}
+                        allowDraggingOfNodes
+                        targetFramerate={60}
+                        objectHighlighted={this.objectHighlighted}
+                        match={this.state.searchString}
+                        viewChanged={this.setView}
+                    />
+                    {
+                        !!connDetails &&
+                        <ConnectionDetails
+                            requestRate={graph.requestRateForConnection(connDetails.split('--')[0], connDetails.split('--')[1])}
+                            errorPercent={graph.errorRateForConnection(connDetails.split('--')[0], connDetails.split('--')[1])}
+                            onClose={this.onConnectionDetailsClose}
+                        />
+                    }
+                </article>
             </div>
         );
     }
